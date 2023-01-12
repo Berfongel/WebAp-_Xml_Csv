285377c35ecc120e7ba6b16870ce6ad2f4d13a71
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-12 15:54:47 +0300

    Add files via upload

---
+++ WebAPI.sql
@@ -0,0 +1,39 @@
+--tablo ekleme
+--CREATE TABLE Data
+--(
+--CityId int identity primary key,
+--CityName varchar(100),
+--CityCode int,
+--DistrictName varchar(100),
+--ZipCode varchar(100)
+--)
+
+
+--tabloyu listeleme
+--select*from Data
+
+--Tabloya veri ekleme
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Aladað','1720');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ceyhan','1920');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ceyhan','1930');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ceyhan','1940');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ceyhan','1950');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ceyhan','1960');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Çukurova','1170');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Çukurova','1360');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Çukurova','1780');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Feke','1660');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Ýmamoðlu','1700');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Karaisalý','1770');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Karataþ','1900');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Kozan','1500');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Kozan','1510');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Kozan','1530');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Kozan','1542');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Kozan','1555');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Pozantý','1470');
+--insert into Data(CityName,CityCode,DistrictName,ZipCode) values('Adana',1,'Pozantý','1490');
+
+
+--güncelleme denemesi 
+--update Data set DistrictName='Çukurova' where CityId=7
---
+++ WebApıXml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebApıXml : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult ApıXml()
+        {
+
+            /*veritabanında bağlantı*/
+            SqlConnection con = new SqlConnection(@"Data Source=KALITE\SQLEXPRESS;Initial Catalog=WebApı;Integrated Security=True");
+            
+                con.Open();
+            /*veritabanındaki tabloyu listeliyor*/
+            SqlCommand com = new SqlCommand("SELECT CityName,CityCode,DistrictName,ZipCode FROM DATA FOR XML PATH('DATA')", con);
+
+            SqlDataAdapter sdp = new SqlDataAdapter(com);
+                    
+                        DataSet ds = new DataSet();
+                        sdp.Fill(ds);
+                        /*xml uzantısı*/
+                        string csvData = XmlWriteMode(ds.Tables[0]);
+                        //StreamReader sr = new StreamReader(csvData,Encoding.GetEncoding(""));
+                        var fileBytes = Encoding.UTF8.GetBytes(csvData);
+                        /*dosyayı hangi uzantıda indirmemiz gerektiği*/
+                        return File(fileBytes, "application/xml", "Data.xml");               
+       
+    }
+        private string XmlWriteMode(DataTable dataTable)
+        {
+
+            StringBuilder csvBuilder = new StringBuilder();
+            IEnumerable<string> columNames = (IEnumerable<string>)dataTable.Columns.Cast<DataColumn>()
+                .Select(x => x.ColumnName);
+            /*kolonlar adları yazılıyor*/
+            csvBuilder.AppendLine(string.Join(" ", columNames));
+            foreach (DataRow row in dataTable.Rows)
+            {
+                IEnumerable<string> file = row.ItemArray.Select
+                    (x => string.Concat("  ", x.ToString().Replace("  ", "   "), " "));
+                csvBuilder.AppendLine(string.Join(' ', file));
+
+            }
+            return csvBuilder.ToString();
+        }
+    }
+}
